335d5c51f516c04e589fed9b3b41e6314be70522
Ivo Yankulovski <ivoyankulovski@gmail.com>
2018-10-08 17:09:47 +0300

    Create Shape.Header.js

---
+++ src/ui/headers/Shape.Header.js
@@ -0,0 +1,2 @@
+ProtoSS.headerSuperList2.push([zetaret.global.packages.clockwork.ui.Shape,["zetaret.global.packages.clockwork.ui::DisplayObject","zetaret.global.packages.clockwork.ui.interfaces::IShape"]]);
+zetaret.global.packages.clockwork.ui.Shape.__xeltoss_eamaps=["zetaret.global.packages.clockwork.ui::DisplayObject"];
