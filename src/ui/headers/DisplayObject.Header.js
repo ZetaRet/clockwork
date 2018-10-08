49dc88a632a164289c0e647b421cde21285386b5
Ivo Yankulovski <ivoyankulovski@gmail.com>
2018-10-08 17:08:37 +0300

    Create DisplayObject.Header.js

---
+++ src/ui/headers/DisplayObject.Header.js
@@ -0,0 +1,3 @@
+ProtoSS.headerSuperList2.push([zetaret.global.packages.clockwork.ui.DisplayObject,["zetaret.global.packages.clockwork.events::EventDispatcher","zetaret.global.packages.clockwork.ui.interfaces::IDisplayObject"]]);
+zetaret.global.packages.clockwork.ui.DisplayObject.__xeltoss_augmentmap={transform:["at"], colortransform:["act"]};
+zetaret.global.packages.clockwork.ui.DisplayObject.__xeltoss_prepare=function(x){x.addEmbedMap(this, {transform:x.argumentKeyMatch(0,"self"),colortransform:x.argumentKeyMatch(0,"self")});};
