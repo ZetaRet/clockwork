e358c0266b927160a8f466dfeb8eaf9bc89c3ce3
Ivo Yankulovski <ivoyankulovski@gmail.com>
2018-10-08 17:10:42 +0300

    Create TextField.Header.js

---
+++ src/ui/headers/TextField.Header.js
@@ -0,0 +1,1 @@
+ProtoSS.headerSuperList2.push([zetaret.global.packages.clockwork.ui.TextField,["zetaret.global.packages.clockwork.ui::Shape","zetaret.global.packages.clockwork.ui.interfaces::ITextField"]]);
