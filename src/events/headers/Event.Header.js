e1456b1f8d6dfd91d23a9ad3a427c7cb1c9c3aef
Ivo Yankulovski <ivoyankulovski@gmail.com>
2018-10-08 17:01:31 +0300

    Create Event.Header.js

---
+++ src/events/headers/Event.Header.js
@@ -0,0 +1,4 @@
+zetaret.global.packages.clockwork.events.Event.setSuper(ZetaRet_Event);
+ProtoSS.headerImplement.push([ZetaRet_Event, "zetaret.global.packages.clockwork.events.interfaces::IEvent"]);
+ZetaRet_Event.__xeltoss_prepare=function(x){x.addEmbedMap(this, {type:x.argumentKeyMatch(),target:x.argumentKeyMatch()});};
+zetaret.global.packages.clockwork.events.Event.__xeltoss_emaps=["ZetaRet_Event"];
