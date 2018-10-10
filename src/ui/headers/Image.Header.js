b801a1e75bad1f84f6f0236d7b8b33ce26c534ef
Ivo Yankulovski <ivoyankulovski@gmail.com>
2018-10-10 09:23:42 +0300

    Create Image.Header.js

---
+++ src/ui/headers/Image.Header.js
@@ -0,0 +1,1 @@
+ProtoSS.headerSuperList2.push([zetaret.global.packages.clockwork.ui.Image,["zetaret.global.packages.clockwork.ui::Shape","zetaret.global.packages.clockwork.ui.interfaces::IImage"]]);
