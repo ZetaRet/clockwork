8d9ecbde2b1bda9287b30b1625bf73003b3edfe8
Ivo Yankulovski <ivoyankulovski@gmail.com>
2018-10-12 14:24:10 +0300

    Create ButtonContainer.Header.js

---
+++ src/ui/headers/ButtonContainer.Header.js
@@ -0,0 +1,1 @@
+ProtoSS.headerSuperList2.push([zetaret.global.packages.clockwork.ui.ButtonContainer,["zetaret.global.packages.clockwork.data::ChildContainer","zetaret.global.packages.clockwork.ui::Button"]]);
