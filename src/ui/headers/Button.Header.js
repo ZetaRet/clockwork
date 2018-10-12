c4bb5538129d786f8b3488169da8d3023c18b2ce
Ivo Yankulovski <ivoyankulovski@gmail.com>
2018-10-12 14:22:52 +0300

    Create Button.Header.js

---
+++ src/ui/headers/Button.Header.js
@@ -0,0 +1,1 @@
+ProtoSS.headerSuperList2.push([zetaret.global.packages.clockwork.ui.Button,["zetaret.global.packages.clockwork.ui::DisplayObject","zetaret.global.packages.clockwork.ui.interfaces::IButton"]]);
