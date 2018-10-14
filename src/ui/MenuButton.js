3a468afbed6b471e1e14abfad68fdfcacf5fc848
Ivo Yankulovski <ivoyankulovski@gmail.com>
2018-10-14 12:16:39 +0300

    Create MenuButton.js

---
+++ src/ui/MenuButton.js
@@ -0,0 +1,19 @@
+window.package("zetaret.global.packages.clockwork.ui").internal(
+function MenuButton(){
+	var o = this, a = arguments;
+	o.tf=new zetaret.global.packages.clockwork.ui.TextField();
+	o.super(a, null);
+	var m = {};
+	m.setText=function(txt){
+		o.tf.setText(txt).renderText();
+		o.dispatchEvent(new zetaret.global.packages.clockwork.events.Event("change"));
+		return o;
+	};
+	m.initMenuButton=function(){
+		o.addChild(o.tf);
+		o.dispatchEvent(new zetaret.global.packages.clockwork.events.Event("initMenuButton"));
+		return o;
+	};
+	o.superize(a, m, true, true);
+	return o;
+});
