43edd1dfc11bb6010c04adb4a6fb9ca1c4f02b62
Ivo Yankulovski <ivoyankulovski@gmail.com>
2018-10-14 22:26:00 +0300

    Create Checkbox.Header.js

---
+++ src/ui/headers/Checkbox.Header.js
@@ -0,0 +1,1 @@
+ProtoSS.headerSuperList2.push([zetaret.global.packages.clockwork.ui.Checkbox,["zetaret.global.packages.clockwork.ui::ButtonContainer","zetaret.global.packages.clockwork.ui.interfaces::ICheckbox"]]);
