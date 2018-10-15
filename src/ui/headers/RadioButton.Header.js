2372c5c86e1b1dd056de240b8c81171d52df5f0a
Ivo Yankulovski <ivoyankulovski@gmail.com>
2018-10-15 11:39:54 +0300

    Create RadioButton.Header.js

---
+++ src/ui/headers/RadioButton.Header.js
@@ -0,0 +1,1 @@
+ProtoSS.headerSuperList2.push([zetaret.global.packages.clockwork.ui.RadioButton,["zetaret.global.packages.clockwork.ui::ButtonContainer","zetaret.global.packages.clockwork.ui.interfaces::IRadioButton"]]);
