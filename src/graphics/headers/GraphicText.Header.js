9c673b09919f388a0e9dc994f77cc306796d14e7
Ivo Yankulovski <ivoyankulovski@gmail.com>
2018-10-16 16:31:04 +0300

    Create GraphicText.Header.js

---
+++ src/graphics/headers/GraphicText.Header.js
@@ -0,0 +1,2 @@
+ProtoSS.headerSuper.push(["zetaret.global.packages.clockwork.graphics.interfaces::IGraphicText","zetaret.global.packages.clockwork.graphics.interfaces::IGraphicCommand"]);
+ProtoSS.headerImplement.push([zetaret.global.packages.clockwork.graphics.GraphicText,"zetaret.global.packages.clockwork.graphics.interfaces::IGraphicText"]);
