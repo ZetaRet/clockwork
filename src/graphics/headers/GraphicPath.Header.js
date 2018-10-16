8def9698d1093821f0fc7fc8ff8410186e424992
Ivo Yankulovski <ivoyankulovski@gmail.com>
2018-10-16 16:55:34 +0300

    Create GraphicPath.Header.js

---
+++ src/graphics/headers/GraphicPath.Header.js
@@ -0,0 +1,2 @@
+ProtoSS.headerSuper.push(["zetaret.global.packages.clockwork.graphics.interfaces::IGraphicPath","zetaret.global.packages.clockwork.graphics.interfaces::IGraphicCommand"]);
+ProtoSS.headerImplement.push([zetaret.global.packages.clockwork.graphics.GraphicPath,"zetaret.global.packages.clockwork.graphics.interfaces::IGraphicPath"]);
