f026e0d4e938181cfc1e5d2b38fb44bbf18fb50e
Ivo Yankulovski <ivoyankulovski@gmail.com>
2018-10-16 15:40:16 +0300

    Create GraphicStroke.Header.js

---
+++ src/graphics/headers/GraphicStroke.Header.js
@@ -0,0 +1,2 @@
+ProtoSS.headerSuper.push(["zetaret.global.packages.clockwork.graphics.interfaces::IGraphicStroke","zetaret.global.packages.clockwork.graphics.interfaces::IGraphicCommand"]);
+ProtoSS.headerImplement.push([zetaret.global.packages.clockwork.graphics.GraphicStroke,"zetaret.global.packages.clockwork.graphics.interfaces::IGraphicStroke"]);
