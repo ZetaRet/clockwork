2b8ece479e7d19482a05507d6672b2ff3048ff35
Ivo Yankulovski <ivoyankulovski@gmail.com>
2018-10-16 21:58:58 +0300

    Create RGB.Header.js

---
+++ src/color/headers/RGB.Header.js
@@ -0,0 +1,1 @@
+zetaret.global.packages.clockwork.color.RGB.__xeltoss_prepare=function(x){x.addEmbedMap(this, {Red:x.argumentKeyMatch(0,0),Green:x.argumentKeyMatch(0,0),Blue:x.argumentKeyMatch(0,0)});};
