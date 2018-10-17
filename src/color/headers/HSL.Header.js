182d066a7743fd32aad2b75f06a19e9d1a152763
Ivo Yankulovski <ivoyankulovski@gmail.com>
2018-10-17 11:49:58 +0300

    Create HSL.Header.js

---
+++ src/color/headers/HSL.Header.js
@@ -0,0 +1,1 @@
+zetaret.global.packages.clockwork.color.HSL.__xeltoss_prepare=function(x){x.addEmbedMap(this, {Hue:x.argumentKeyMatch(0,0),Saturation:x.argumentKeyMatch(0,0),Luminosity:x.argumentKeyMatch(0,0)});};
