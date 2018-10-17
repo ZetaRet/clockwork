4757f078260d0406024364ce8ac7abf00f991287
Ivo Yankulovski <ivoyankulovski@gmail.com>
2018-10-17 10:15:49 +0300

    Create ARGB.Header.js

---
+++ src/color/headers/ARGB.Header.js
@@ -0,0 +1,3 @@
+ProtoSS.headerSuper.push([zetaret.global.packages.clockwork.color.ARGB, "zetaret.global.packages.clockwork.color::RGB"]);
+zetaret.global.packages.clockwork.color.ARGB.__xeltoss_emaps=["zetaret.global.packages.clockwork.color::RGB"];
+zetaret.global.packages.clockwork.color.ARGB.__xeltoss_prepare=function(x){x.addEmbedMap(this, {Alpha:x.argumentKeyMatch(0,0)});};
