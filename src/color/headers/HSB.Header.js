1b743a9568fdbe9b7a2bfbc52c924be63bf02a4b
Ivo Yankulovski <ivoyankulovski@gmail.com>
2018-10-17 13:46:55 +0300

    Create HSB.Header.js

---
+++ src/color/headers/HSB.Header.js
@@ -0,0 +1,1 @@
+zetaret.global.packages.clockwork.color.HSB.__xeltoss_prepare=function(x){x.addEmbedMap(this, {Hue:x.argumentKeyMatch(0,0),Saturation:x.argumentKeyMatch(0,0),Brightness:x.argumentKeyMatch(0,0)});};
