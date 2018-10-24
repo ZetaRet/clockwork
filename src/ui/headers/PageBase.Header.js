84c59b803fa9b57cb2c0334cd699eaf948f40035
Ivo Yankulovski <ivoyankulovski@gmail.com>
2018-10-24 14:42:18 +0300

    Create PageBase.Header.js

---
+++ src/ui/headers/PageBase.Header.js
@@ -0,0 +1,1 @@
+ProtoSS.headerSuperList2.push([zetaret.global.packages.clockwork.ui.PageBase,["zetaret.global.packages.clockwork.data::ChildContainer","zetaret.global.packages.clockwork.ui::DisplayObject"]]);
