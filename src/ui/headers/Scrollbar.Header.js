099fc00c70f62219e82275c6f6362ff16d5e2885
Ivo Yankulovski <ivoyankulovski@gmail.com>
2018-10-24 11:01:54 +0300

    Create Scrollbar.Header.js

---
+++ src/ui/headers/Scrollbar.Header.js
@@ -0,0 +1,1 @@
+ProtoSS.headerSuperList2.push([zetaret.global.packages.clockwork.ui.Scrollbar,["zetaret.global.packages.clockwork.ui::DisplayObjectContainer","zetaret.global.packages.clockwork.ui.interfaces::IScrollbar"]]);
