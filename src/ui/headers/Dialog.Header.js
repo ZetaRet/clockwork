5e157f5e199cc8e79cf0f02c6c01b1a1823e69aa
Ivo Yankulovski <ivoyankulovski@gmail.com>
2018-10-25 14:22:10 +0300

    Create Dialog.Header.js

---
+++ src/ui/headers/Dialog.Header.js
@@ -0,0 +1,1 @@
+ProtoSS.headerSuperList2.push([zetaret.global.packages.clockwork.ui.Dialog,["zetaret.global.packages.clockwork.ui::ButtonContainer","zetaret.global.packages.clockwork.ui.interfaces::IDialog"]]);
