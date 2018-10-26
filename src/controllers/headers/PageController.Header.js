93aec0109e90220ff84b1db9acc26f1a8f3aed33
Ivo Yankulovski <ivoyankulovski@gmail.com>
2018-10-26 21:51:55 +0300

    Create PageController.Header.js

---
+++ src/controllers/headers/PageController.Header.js
@@ -0,0 +1,2 @@
+ProtoSS.headerSuper.push([zetaret.global.packages.clockwork.controllers.PageController,"zetaret.global.packages.clockwork.events::EventDispatcher"]);
+zetaret.global.packages.clockwork.controllers.PageController.__xeltoss_prepare=function(x){x.addEmbedMap(this, {Container:x.argumentKeyMatch(0,"self")});};
