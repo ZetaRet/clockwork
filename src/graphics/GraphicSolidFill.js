544857093d14c8d0c555453fc13f2a279db16296
Ivo Yankulovski <ivoyankulovski@gmail.com>
2018-10-27 14:29:44 +0300

    Create GraphicSolidFill.js

---
+++ src/graphics/GraphicSolidFill.js
@@ -0,0 +1,14 @@
+window.package("zetaret.global.packages.clockwork.graphics").internal(
+function GraphicSolidFill() {
+	var o = this,a=arguments;
+	o.color=0;
+	o.super(a, true);
+	o.command=zetaret.global.packages.clockwork.graphics.GraphicCommand.TYPES.SOLID_FILL;
+	var m = {};
+	m.toCommandData=function(){
+		return [o.command, [o.color]];
+	};
+	o.superize(a, m, true, true);
+	return o;
+}
+);
