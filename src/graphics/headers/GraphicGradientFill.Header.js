7a4524470e8777919ede68b07af39e4caea9005c
Ivo Yankulovski <ivoyankulovski@gmail.com>
2018-10-27 14:24:26 +0300

    Create GraphicGradientFill.Header.js

---
+++ src/graphics/headers/GraphicGradientFill.Header.js
@@ -0,0 +1,2 @@
+ProtoSS.headerSuper.push(["zetaret.global.packages.clockwork.graphics.interfaces::IGraphicGradientFill","zetaret.global.packages.clockwork.graphics.interfaces::IGraphicCommand"]);
+ProtoSS.headerImplement.push([zetaret.global.packages.clockwork.graphics.GraphicGradientFill,"zetaret.global.packages.clockwork.graphics.interfaces::IGraphicGradientFill"]);
