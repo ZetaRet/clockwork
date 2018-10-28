7b031787f993a29b69b75cedd832d58a5f9f1c19
Ivo Yankulovski <ivoyankulovski@gmail.com>
2018-10-28 12:05:28 +0200

    Create GraphicBitmapFill.Header.js

---
+++ src/graphics/headers/GraphicBitmapFill.Header.js
@@ -0,0 +1,2 @@
+ProtoSS.headerSuper.push(["zetaret.global.packages.clockwork.graphics.interfaces::IGraphicBitmapFill","zetaret.global.packages.clockwork.graphics.interfaces::IGraphicCommand"]);
+ProtoSS.headerImplement.push([zetaret.global.packages.clockwork.graphics.GraphicBitmapFill,"zetaret.global.packages.clockwork.graphics.interfaces::IGraphicBitmapFill"]);
