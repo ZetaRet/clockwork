fe471b4d24635521d9a2039aa5a6658cf26f3b83
Ivo Yankulovski <ivoyankulovski@gmail.com>
2018-11-03 17:09:59 +0200

    Create Label.Header.js

---
+++ src/ui/headers/Label.Header.js
@@ -0,0 +1,1 @@
+ProtoSS.headerSuperList2.push([zetaret.global.packages.clockwork.ui.Label,["zetaret.global.packages.clockwork.ui::DisplayObjectContainer","zetaret.global.packages.clockwork.ui.interfaces::ILabel"]]);
