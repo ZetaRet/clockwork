d84daf5b94409d9a84fc0b93aa33c0893c820f35
Ivo Yankulovski <ivoyankulovski@gmail.com>
2018-11-04 14:50:29 +0200

    Create Window.Header.js

---
+++ src/ui/headers/Window.Header.js
@@ -0,0 +1,1 @@
+ProtoSS.headerSuperList2.push([zetaret.global.packages.clockwork.ui.Window,["zetaret.global.packages.clockwork.ui::DisplayObjectContainer","zetaret.global.packages.clockwork.ui.interfaces::IWindow"]]);
