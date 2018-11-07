c5c2297a373fb628b23e10dd4dfd253dcfd605a5
Ivo Yankulovski <ivoyankulovski@gmail.com>
2018-11-07 17:55:25 +0200

    Create GraphicShaderFill.Header.js

---
+++ src/graphics/headers/GraphicShaderFill.Header.js
@@ -0,0 +1,2 @@
+ProtoSS.headerSuper.push(["zetaret.global.packages.clockwork.graphics.interfaces::IGraphicShaderFill","zetaret.global.packages.clockwork.graphics.interfaces::IGraphicCommand"]);
+ProtoSS.headerImplement.push([zetaret.global.packages.clockwork.graphics.GraphicShaderFill,"zetaret.global.packages.clockwork.graphics.interfaces::IGraphicShaderFill"]);
