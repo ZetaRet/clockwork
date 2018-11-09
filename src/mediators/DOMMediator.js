43f3f639e9e17067a81ca67fa79c4d413ec6e415
Ivo Yankulovski <ivoyankulovski@gmail.com>
2018-11-09 14:11:34 +0200

    Create DOMMediator.js

---
+++ src/mediators/DOMMediator.js
@@ -0,0 +1,23 @@
+window.package("zetaret.global.packages.clockwork.mediators").internal(
+function DOMMediator(obj,domo) {
+	var o = this,a=arguments;
+	o.obj=obj;
+	o.domo=domo;
+	o.super(a, true);
+	var m={};
+	m.eventize=function(e1,e2,cls,fname,h,obj,domo){
+		if (!obj)obj=o.obj;
+		if (!domo)domo=o.domo;
+		var f=function(e){
+			var ee=new cls();
+			ee.type=e2;
+			ee.setNativeEvent(e);
+			if (h)h(obj,domo,ee);
+			obj[fname](ee);
+		};
+		domo.addEventListener(e1,f);
+		return f;
+	};
+	o.superize(a,m,true,true);
+	return o;
+});
