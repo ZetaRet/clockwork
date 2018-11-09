adebc846edb35f5be80e6b257bc8d7235cb25e6f
Ivo Yankulovski <ivoyankulovski@gmail.com>
2018-11-09 14:12:02 +0200

    Create DOMMediator.Header.js

---
+++ src/mediators/headers/DOMMediator.Header.js
@@ -0,0 +1,2 @@
+ProtoSS.headerSuper.push([zetaret.global.packages.clockwork.mediators.DOMMediator,"zetaret.global.packages.clockwork.events::EventDispatcher"]);
+zetaret.global.packages.clockwork.mediators.DOMMediator.__xeltoss_prepare=function(x){x.addEmbedMap(this, {obj:x.argumentKeyMatch(),domo:x.argumentKeyMatch()});};
