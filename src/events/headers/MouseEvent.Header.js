44103a58abca0b975aad75cdf557d64eef0c13e5
Ivo Yankulovski <ivoyankulovski@gmail.com>
2018-12-04 18:12:10 +0200

    Create MouseEvent.Header.js

---
+++ src/events/headers/MouseEvent.Header.js
@@ -0,0 +1,2 @@
+ProtoSS.headerSuperList2.push([zetaret.global.packages.clockwork.events.MouseEvent,["zetaret.global.packages.clockwork.events::Event","zetaret.global.packages.clockwork.events.interfaces::IMouseEvent"]]);
+zetaret.global.packages.clockwork.events.MouseEvent.__xeltoss_emaps=["ZetaRet_Event"];
