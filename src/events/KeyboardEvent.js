cee1408a163ab91b8f536a7d8560a5bc55090f62
Ivo Yankulovski <ivoyankulovski@gmail.com>
2019-03-11 17:24:40 +0200

    Update KeyboardEvent.js

--- src/events/KeyboardEvent.js
+++ src/events/KeyboardEvent.js
@@ -1,28 +1,28 @@
 window.package("zetaret.global.packages.clockwork.events").internal(
 function KeyboardEvent(type, target) {
-	var o = this,a=arguments;
-	o.key="";
-	o.code="";
-	o.keyCode=0;
-	o.super(a,true);
+	var o = this,
+		a = arguments;
+	o.key = "";
+	o.code = "";
+	o.keyCode = 0;
+	o.super(a, true);
 	var m = {};
-	m.getKey=function(){
+	m.getKey = function() {
 		return o.key;
 	};
-	m.getCode=function(){
+	m.getCode = function() {
 		return o.code;
 	};
-	m.getKeyCode=function(){
+	m.getKeyCode = function() {
 		return o.keyCode;
 	};
-	m.setNativeEvent=function(e){
-		o.nativeEvent=e;
-		o.key=e.key;
-		o.code=e.code;
-		o.keyCode=e.keyCode||e.which;
+	m.setNativeEvent = function(e) {
+		o.nativeEvent = e;
+		o.key = e.key;
+		o.code = e.code;
+		o.keyCode = e.keyCode || e.which;
 		return o;
 	};
 	o.superize(a, m, true, true);
 	return o;
-}
-);
+});
