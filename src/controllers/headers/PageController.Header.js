904acd42eb50db05b7b178349c595ffdd967de21
Ivo Yankulovski <ivoyankulovski@gmail.com>
2019-03-11 13:30:05 +0200

    Update PageController.Header.js

--- src/controllers/headers/PageController.Header.js
+++ src/controllers/headers/PageController.Header.js
@@ -1,2 +1,6 @@
-ProtoSS.headerSuper.push([zetaret.global.packages.clockwork.controllers.PageController,"zetaret.global.packages.clockwork.events::EventDispatcher"]);
-zetaret.global.packages.clockwork.controllers.PageController.__xeltoss_prepare=function(x){x.addEmbedMap(this, {Container:x.argumentKeyMatch(0,"self")});};
+ProtoSS.headerSuper.push([zetaret.global.packages.clockwork.controllers.PageController, "zetaret.global.packages.clockwork.events::EventDispatcher"]);
+zetaret.global.packages.clockwork.controllers.PageController.__xeltoss_prepare = function(x) {
+	x.addEmbedMap(this, {
+		Container: x.argumentKeyMatch(0, "self")
+	});
+};
