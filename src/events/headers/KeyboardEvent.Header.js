485deaf6603cc1a836ac3f54a3dd188fcd4d2351
Ivo Yankulovski <ivoyankulovski@gmail.com>
2019-03-11 17:26:48 +0200

    Update KeyboardEvent.Header.js

--- src/events/headers/KeyboardEvent.Header.js
+++ src/events/headers/KeyboardEvent.Header.js
@@ -1,2 +1,5 @@
-ProtoSS.headerSuperList2.push([zetaret.global.packages.clockwork.events.KeyboardEvent,["zetaret.global.packages.clockwork.events::Event","zetaret.global.packages.clockwork.events.interfaces::IKeyboardEvent"]]);
-zetaret.global.packages.clockwork.events.KeyboardEvent.__xeltoss_emaps=["ZetaRet_Event"];
+ProtoSS.headerSuperList2.push([zetaret.global.packages.clockwork.events.KeyboardEvent, [
+	"zetaret.global.packages.clockwork.events::Event",
+	"zetaret.global.packages.clockwork.events.interfaces::IKeyboardEvent"
+]]);
+zetaret.global.packages.clockwork.events.KeyboardEvent.__xeltoss_emaps = ["ZetaRet_Event"];
