4e8ecbedfdf3433a934f6b589d66321d759968b2
Ivo Yankulovski <ivoyankulovski@gmail.com>
2019-03-11 17:27:11 +0200

    Update MouseEvent.Header.js

--- src/events/headers/MouseEvent.Header.js
+++ src/events/headers/MouseEvent.Header.js
@@ -1,2 +1,5 @@
-ProtoSS.headerSuperList2.push([zetaret.global.packages.clockwork.events.MouseEvent,["zetaret.global.packages.clockwork.events::Event","zetaret.global.packages.clockwork.events.interfaces::IMouseEvent"]]);
-zetaret.global.packages.clockwork.events.MouseEvent.__xeltoss_emaps=["ZetaRet_Event"];
+ProtoSS.headerSuperList2.push([zetaret.global.packages.clockwork.events.MouseEvent, [
+	"zetaret.global.packages.clockwork.events::Event",
+	"zetaret.global.packages.clockwork.events.interfaces::IMouseEvent"
+]]);
+zetaret.global.packages.clockwork.events.MouseEvent.__xeltoss_emaps = ["ZetaRet_Event"];
