b89d48300b50c680df01f444217422d654e536d2
ivoyankulovski <ivoyankulovski@gmail.com>
2019-03-11 21:02:54 +0200

    *beautify

--- src/interfaces/IGraphics.js
+++ src/interfaces/IGraphics.js
@@ -1,15 +1,29 @@
-window.package("zetaret.global.packages.clockwork.interfaces").IGraphics={
-	IGraphicBitmapFill:{},
-	IGraphicCommand:{toCommandData:[]},
-	IGraphicData:{},
-	IGraphicEndFill:{},
-	IGraphicFill:{},
-	IGraphicGradientFill:{computeGradient:[]},
-	IGraphicPath:{moveTo:[Number,Number], lineTo:[Number,Number], curveTo:[Number,Number,Number,Number], cubicCurveTo:[Number,Number,Number,Number,Number,Number], arc:[Number,Number,Number,Number,Number,Boolean], arcTo:[Number,Number,Number,Number,Number], rect:[Number,Number,Number,Number], circle:[Number,Number,Number], ellipse:[Number,Number,Number,Number,Number,Number,Number,Boolean]},
-	IGraphicShaderFill:{},
-	IGraphicSolidFill:{},
-	IGraphicStroke:{},
-	IGraphicText:{},
-	IGraphicTrianglePath:{},
-	IGraphics:{}
+window.package("zetaret.global.packages.clockwork.interfaces").IGraphics = {
+	IGraphicBitmapFill: {},
+	IGraphicCommand: {
+		toCommandData: []
+	},
+	IGraphicData: {},
+	IGraphicEndFill: {},
+	IGraphicFill: {},
+	IGraphicGradientFill: {
+		computeGradient: []
+	},
+	IGraphicPath: {
+		moveTo: [Number, Number],
+		lineTo: [Number, Number],
+		curveTo: [Number, Number, Number, Number],
+		cubicCurveTo: [Number, Number, Number, Number, Number, Number],
+		arc: [Number, Number, Number, Number, Number, Boolean],
+		arcTo: [Number, Number, Number, Number, Number],
+		rect: [Number, Number, Number, Number],
+		circle: [Number, Number, Number],
+		ellipse: [Number, Number, Number, Number, Number, Number, Number, Boolean]
+	},
+	IGraphicShaderFill: {},
+	IGraphicSolidFill: {},
+	IGraphicStroke: {},
+	IGraphicText: {},
+	IGraphicTrianglePath: {},
+	IGraphics: {}
 };
