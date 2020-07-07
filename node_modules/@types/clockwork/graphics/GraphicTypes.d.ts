76f86c39a6c880c05ada17126fbae67ab84ba136
Ivo Yankulovski <ivoyankulovski@gmail.com>
2020-07-07 16:45:30 +0300

    Update d.ts

---
+++ node_modules/@types/clockwork/graphics/GraphicTypes.d.ts
@@ -0,0 +1,7 @@
+declare namespace zetaret.global.packages.clockwork.graphics {
+	export type FillType = number | string | CanvasGradient | CanvasPattern;
+	export type ColorType = number | string;
+	export type CanvasType = CanvasGradient | CanvasPattern;
+	export type BitmapType = string | CanvasImageSource;
+	export type GraphicType = GraphicCommand | GraphicData;
+}
