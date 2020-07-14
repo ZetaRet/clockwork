e0b480d3de459f7e2804e9a15d5a8391af641b8c
Ivo Yankulovski <ivoyankulovski@gmail.com>
2020-07-14 16:06:22 +0300

    Update headers

---
+++ src/events/headers/EventTarget.Header.js
@@ -0,0 +1,4 @@
+ProtoSS.headerSuperList2.push([zetaret.global.packages.clockwork.events.EventTarget, [
+	"zetaret.global.packages.clockwork.events::Event",
+	"zetaret.global.packages.clockwork.events.interfaces::IEventTarget"
+]]);
